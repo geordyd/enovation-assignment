3d3cd965e52b07b4ace495587fe778575c62a11c
geordyd <geordycjd@gmail.com>
2022-07-01 02:27:32 +0200

    Added database connection and migrations

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using enovation.Models;
+
+namespace enovation.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Person> Persons { get; set; }
+
+    }
+
+}
